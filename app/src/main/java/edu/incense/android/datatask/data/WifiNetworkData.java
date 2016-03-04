2db3f6614918a85db91a1e41119bed97e0929a05
Ivan Felix <rogelio.felix@gmail.com>
2016-03-03 22:56:23 -0700

    Added a class that represent the list of available wifi networks at a given time  Changes to be committed: 	new file:   app/src/main/java/edu/incense/android/datatask/data/WifiNetworkData.java

---
+++ app/src/main/java/edu/incense/android/datatask/data/WifiNetworkData.java
@@ -0,0 +1,50 @@
+package edu.incense.android.datatask.data;
+
+// Android libraries.
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+// Java libraries
+import java.util.*;
+/**
+ * Created by xilef on 3/3/2016.
+ *
+ * This class is used to represent the available wifi networks at a given time.
+ */
+public class WifiNetworkData extends Data{
+
+    // This class represents a single wifi access point.
+    public class WifiNetworkItem{
+        public String bssid; // The mac address of the access point.
+        public String ssid; // The name of the access point.
+        public int frequency; // The frequency of the channel the client is using to communicate.
+        public int rssi; // The level of the signal the client is getting.
+
+        public WifiNetworkItem(ScanResult scanResult){
+            this.bssid = scanResult.BSSID;
+            this.ssid = scanResult.SSID;
+            this.frequency = scanResult.frequency;
+            this.rssi = scanResult.level;
+        }
+    }
+
+    private List<WifiNetworkItem> wifiNetworks;
+
+    public WifiNetworkData(){
+        super(DataType.WIFI);
+        wifiNetworks = new LinkedList<WifiNetworkItem>();
+    }
+
+    public WifiNetworkData(ScanResult scanResult){
+        super(DataType.WIFI);
+        wifiNetworks = new LinkedList<WifiNetworkItem>();
+        WifiNetworkItem newItem = new WifiNetworkItem(scanResult);
+
+        wifiNetworks.add(newItem);
+    }
+
+    public void addWifiNetworkItem(ScanResult scanResult){
+        WifiNetworkItem newItem = new WifiNetworkItem(scanResult);
+        wifiNetworks.add(newItem);
+    }
+}
